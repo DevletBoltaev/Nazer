cee8549d05afd4ccb982bb79ba83d83555b6a174
richardnazer <112823851+richardnazer@users.noreply.github.com>
2022-09-11 19:01:47 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,8 +47,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Мой Телеграм", "t.me/Nazer_Ginx")
-    buttons.buildbutton("Мой ВК", "vk.com/devlet_b")
+    buttons.buildbutton("Мой Телеграм", "t.me")
+    buttons.buildbutton("Мой ВК", "vk.com")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
